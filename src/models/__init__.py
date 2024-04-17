381990681509d167654f2c8d3a91df00ea37e852
zeyadmahmoudd <119884511+zeyadmahmoudd@users.noreply.github.com>
2024-04-17 20:42:29 +0100

    Update __init__.py

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -8,7 +8,7 @@ from .resnet import (
     resnet50,
     resnet50_fc512,
 )
-from .tvmodels import mobilenet_v3_small, vgg16, vit_b_16
+from .tvmodels import mobilenet_v3_small, vgg16, googlenet, densenet161
 
 
 __model_factory = {
@@ -21,7 +21,9 @@ __model_factory = {
     "resnet50_fc512": resnet50_fc512,
     "mobilenet_v3_small": mobilenet_v3_small,
     "vgg16": vgg16,
-    "vit_b_16": vit_b_16,
+    "googlenet": googlenet,
+    "densenet161" : densenet,
+
 }
 
 
